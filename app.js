c0ae5b12686165034b643822a7035f46148170b7
Aleksandar Ljubišić <simke20@yahoo.com>
2018-09-14 15:40:30 +0200

    Events

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 new Vue({
   el: "#vue-app",
   data: {
-    name: "Aleksandar",
-    job: "Vue ninja :)",
-    website: "http://aleksander.napravi.online/",
-    websiteTag:
-      '<a href="http://aleksander.napravi.online" target="_blank">Aleksandar portfolio</a>'
+    age: 33,
+    x: 0,
+    y: 0
   },
   methods: {
-    greet: function(time) {
-      return "Good" + " " + time + " " + this.name;
+    add: function(inc) {
+      this.age += inc;
+    },
+    subtract: function(dec) {
+      this.age -= dec;
+    },
+    updateXY: function(event) {
+      //console.log(event);
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     }
   }
 });
