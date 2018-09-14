95e2a1c4448c0880acceb2c6868793c9034dc02e
Aleksandar Ljubišić <simke20@yahoo.com>
2018-09-14 14:47:41 +0200

    Data & Methods

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 new Vue({
   el: "#vue-app",
   data: {
-    name: "Aleksandar"
+    name: "Aleksandar",
+    job: "Web Developer",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo, consequatur."
+  },
+  methods: {
+    greet: function(time) {
+      return "Good" + " " + time + " " + this.name;
+    }
   }
 });
