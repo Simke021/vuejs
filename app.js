c274ddbb0c95faff04c45a14521198cb5701e69e
Aleksandar Ljubišić <simke20@yahoo.com>
2018-09-14 16:35:57 +0200

    Computed Properties

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 new Vue({
   el: "#vue-app",
   data: {
-    name: "",
-    age: ""
+    age: 33,
+    a: 0,
+    b: 0
   },
   methods: {
-    logName: function() {
-      this.name;
+    /*addToA: function() {
+      //console.log("addToA");
+      return this.a + this.age;
     },
-    logAge: function() {
-      this.age;
+    addToB: function() {
+      //console.log("addToB");
+      return this.b + this.age;
+    }*/
+  },
+  computed: {
+    addToA: function() {
+      //console.log("addToA");
+      return this.a + this.age;
+    },
+    addToB: function() {
+      //console.log("addToB");
+      return this.b + this.age;
     }
   }
 });
